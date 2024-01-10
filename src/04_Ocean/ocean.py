b651cdf373d7e8705f076eb57cadae295bedd795
MariaPobokova <146301444+MariaPobokova@users.noreply.github.com>
2024-01-11 01:18:22 +1000

    Update ocean.py

--- src/04_Ocean/ocean.py
+++ src/04_Ocean/ocean.py
@@ -30,7 +30,7 @@ class Ocean:
                     neighbours = [
                         (i - 1, j - 1),
                         (i -  1, j),
-                        (i - 1, j = 1),
+                        (i - 1, j + 1),
                         (i, j - 1),
                         (i, j + 1),
                         (i + 1, j - 1),
