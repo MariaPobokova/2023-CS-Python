225d6e1cbceb37cfef804f2013fe6cd9aed431d4
MariaPobokova <146301444+MariaPobokova@users.noreply.github.com>
2024-01-11 12:30:49 +1000

    Update test_ocean.py

--- src/04_Ocean/test_ocean.py
+++ src/04_Ocean/test_ocean.py
@@ -15,14 +15,14 @@ class Test0cean:
         [0, 0, 1, 3, 3],
     ]
     ocean = Ocean(init_state)
-    
+
     def test_ocean_init(self):
-        assert str(self.ocean) == "\n".join(["".join(str(el) for el in row)
+        assert str(self.ocean) == "\n".join(["".join(str(el) for el in row) 
                                             for row in self.init_state])
     def test_ocean_repr(self):
-        assert self.ocean.__repr__() == f"Ocean({self.init_state!r})" 
+        assert self.ocean.__repr__() == f"Ocean({self.init_state!r})"
 
     def test_ocean_step(self):
         self.ocean = self.ocean.gen_next_quantum()
-        assert str(self.ocean) == "\n".join(["".join(str(el) for el in row)
-                                             for row in self.next_state])
+        assert str(self.ocean) == "\n".join(["".join(str(el) for el in row) 
+                                            for row in self.next_state])
